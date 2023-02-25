14be6531f2eaed338117f06c97088885a344abdc
XENOXI <shifer.k0d@gmail.com>
2023-02-25 09:00:13 +0000

    add deleting

--- task1/CPU.c
+++ task1/CPU.c
@@ -2,17 +2,25 @@
 #include <malloc.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <time.h>
 #define N 10000000
 
 int main()
 {
-    double* arr = (double*)malloc(sizeof(double)*N);
-    double period = 2*M_PI/N;
+    clock_t start = clock();
+    float* arr = (float*)malloc(sizeof(float)*N);
+    float period = 2*M_PI/N;
     for (int i =0;i<N;i++)
         arr[i]=sin(period*i);
-    double res = 0;
+    printf("first cycle time,s: %f\n",(float)(clock()-start)/CLOCKS_PER_SEC);
+    start = clock();
+    float res = 0;
     for (int i =0; i<N;i++)
         res += arr[i];
+    
+    printf("second cycle time,s: %f\n",(float)(clock()-start)/CLOCKS_PER_SEC);
 
-    printf("res: %.20lf\n",res);
+    printf("res: %.20f\n",res);
+
+    free(arr);
 }
--- task1/GPU.c
+++ task1/GPU.c
@@ -2,23 +2,25 @@
 #include <malloc.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <time.h>
 #define N 10000000
 
 int main()
 {
     double* arr = (double*)malloc(sizeof(double)*N); 
     double period = 2*M_PI/N;
+    clock_t start = clock();
 #pragma acc data create(arr[0:N])
   {
 #pragma acc parallel loop
     for (int i =0;i<N;i++)
         arr[i]=sin(period*i);
     double res = 0;
-
+    printf("first cycle time,s: %f\n",(float)(clock()-start)/CLOCKS_PER_SEC);
 #pragma acc parallel loop,copy(res)
     for (int i =0; i<N;i++)
         res += arr[i];
-
+    printf("second cycle time,s: %f\n",(float)(clock()-start)/CLOCKS_PER_SEC);
     printf("res: %.20f\n",res);
 
   }
