7101a95617b8a21a32892fe1cc1e29053f5053aa
XENOXI <shifer.k0d@gmail.com>
2023-05-02 06:57:37 +0000

    task6

---
+++ task6/nueron.cu
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cuda.h>
+#include <cuda-runtime.h>
+#include "cublas_v2.h"
+#include <string>
+#include <fstream>
+
+float* get_array(std::string filepath)
+{   
+    std::ifstream file(filepath);
+    while (file.)
+    {
+
+    }
+}
